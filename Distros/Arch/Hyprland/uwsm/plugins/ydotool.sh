652fb6b874d4f2c39d8abd65128f6ae4769b07c5
phaedrusflow <map@qompass.ai>
2025-05-17 12:58:09 -0700

    Update metadata, badge, and citation for 2025-05-17

--- Distros/Arch/Hyprland/uwsm/plugins/ydotool.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/false
-
-quirks__hyprland() {
-  export YDOTOOL_SOCKET=/run/user/$(id -u)/.ydotool_socket
-  
-  # Make sure the variable is exported to activation environments
-  UWSM_FINALIZE_VARNAMES="${UWSM_FINALIZE_VARNAMES}${UWSM_FINALIZE_VARNAMES:+ }YDOTOOL_SOCKET"
-}
-
--- Distros/Linux-Rust/Zoxide/init.fish
+++
@@ -1,5 +0,0 @@
-if command -sq zoxide
-    zoxide init fish | source
-else
-    echo 'zoxide: command not found, please install it from https://github.com/ajeetdsouza/zoxide'
-end
